0f9f27df2b810117a6fcd9743e89895479d96d8f
lizuncong <2900170896@qq.com>
2025-04-11 11:13:32 +0800

    feat: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import Button from '@/components/Button';
+
 import styles from './App.module.css';
 function App() {
-  return <div className={styles.container}>入口</div>;
+  return (
+    <div className={styles.container}>
+      入口<Button></Button>
+    </div>
+  );
 }
 
 export default App;
