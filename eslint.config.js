0ad960019925df04a6ee36c5537dc49204d210fe
lizuncong <2900170896@qq.com>
2025-04-11 10:48:25 +0800

    feat: unused import

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import reactX from 'eslint-plugin-react-x';
 import reactDom from 'eslint-plugin-react-dom';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+
 // 如果项目使用的ESLint可能会出现与 Prettier 的格式化规则冲突的情况。
 // 为了避免这种情况,可以使用 eslint-config-prettier 来禁用所有与 Prettier 冲突的 ESLint 规则 。
 // 不过截止目前还没发现有冲突的地方，因此先不用加这个
@@ -35,6 +38,8 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
       'react-x': reactX,
       'react-dom': reactDom,
+      'unused-imports': unusedImports,
+      'simple-import-sort': simpleImportSort,
       // 'prettier',
     },
     rules: {
@@ -45,6 +50,19 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-non-null-assertion': 'off',
       'react-hooks/exhaustive-deps': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 );
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,6 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import styles from './index.module.less';
-import './index.less';
+import styles from './App.module.css';
 function App() {
-  const [count, setCount] = useState(0);
-  const a = 1;
-
-  useEffect(() => {
-    console.log(a);
-    console.log(count, a);
-  }, [count]);
-
-  return (
-    <>
-      <div className={styles.container}>
-        <a className="dfd" href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <div className={styles.container}>入口</div>;
 }
 
 export default App;
