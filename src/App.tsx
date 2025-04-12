34e3f25e377cbf9a775529154988bb132cd44a21
lizuncong <2900170896@qq.com>
2025-04-12 23:46:33 +0800

    feat: stash

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/normalize.css';
 
 import i18next from 'i18next';
+import { useEffect } from 'react';
 import { initReactI18next } from 'react-i18next';
 import { RouterProvider } from 'react-router';
 
@@ -17,6 +18,22 @@ i18next.use(initReactI18next).init({
   },
 });
 function App() {
+  useEffect(() => {
+    console.log('监听副页面');
+    // 接收来自父页面的消息
+    window.addEventListener('message', function (event) {
+      // 验证消息来源
+      // if (event.origin !== 'https://parentdomain.com') {
+      //   console.warn('来自未知来源的消息：', event.origin);
+      //   return;
+      // }
+      // 处理消息
+      console.log('收到来自父页面的消息：', event.data);
+      // 回复父页面
+      const reply = { type: 'RESPONSE', text: '你好，父页面！' };
+      event.source!.postMessage(reply, event.origin as never);
+    });
+  }, []);
   return <RouterProvider router={router} />;
 }
 
