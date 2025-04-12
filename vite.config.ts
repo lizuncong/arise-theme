2057b88fcd5171e470c27759a9d62f8e4d497cb5
lizuncong <2900170896@qq.com>
2025-04-12 23:23:30 +0800

    feat: stash

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
 const srcP = path.resolve(__dirname, './src');
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': srcP,
+export default (params: { mode: string }) => {
+  return defineConfig({
+    base: params.mode === 'development' ? '' : '/arise-theme/',
+
+    plugins: [svgr(), react()],
+    resolve: {
+      alias: {
+        '@': srcP,
+      },
     },
-  },
-});
+  });
+};
