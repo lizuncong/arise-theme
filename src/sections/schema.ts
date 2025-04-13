0a921f5ee24cde83351ec86da83ed7421536cf8c
lizuncong <2900170896@qq.com>
2025-04-13 15:22:17 +0800

    feat: 大小写的问题

--- src/sections/schema.ts
+++ src/sections/schema.ts
@@ -1,6 +1,6 @@
 import featureSliceShowSchema from './FeaturedSlideShow/schema.json';
 import spliceSchema from './MultiMediaSplicing/schema.json';
-import videoSchema from './video/schema.json';
+import videoSchema from './Video/schema.json';
 
 export const allSectionSchema = {
   'featured-slideshow': featureSliceShowSchema,
