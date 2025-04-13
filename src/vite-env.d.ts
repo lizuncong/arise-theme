1697a01c49c058065919f99b1e8c6939f298456e
lizuncong <2900170896@qq.com>
2025-04-13 15:19:23 +0800

    feat: 类型声明

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg?react' {
+  const str: string;
+  export default str;
+}
+
+declare module '*.json' {
+  const jsonObj: any;
+  export default jsonObj;
+}
