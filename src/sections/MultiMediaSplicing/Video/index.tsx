ebe71231c378f78ce05b763207cfaffb8f04434a
lizuncong <2900170896@qq.com>
2025-04-24 00:03:39 +0800

    feat: stash

--- src/sections/MultiMediaSplicing/Video/index.tsx
+++ src/sections/MultiMediaSplicing/Video/index.tsx
@@ -1,13 +1,26 @@
 import { memo } from 'react';
 
+import DefaultVideo from '@/assets/default.svg?react';
+
+import styles from './index.module.less';
 interface VideoBlockProps {
   className?: string;
   settings: Record<string, any>;
 }
 const VideoBlock = memo((props: VideoBlockProps) => {
   const { className, settings } = props;
-  console.log('VideoBlock=======', settings);
-  return <div className={className}>视频 block</div>;
+  const cover = settings.cover_image?.value as { url: string; origin: string };
+  const externalVideoUrl = settings.external_video?.value as string;
+  const internalVideoUrl = settings.internal_video?.value as string;
+  return (
+    <div className={[styles.block, className].join(' ')}>
+      {externalVideoUrl && <video muted controls src={externalVideoUrl}></video>}
+      {internalVideoUrl && <video muted controls src={internalVideoUrl}></video>}
+      {!externalVideoUrl &&
+        !internalVideoUrl &&
+        (cover?.url ? <img src={cover?.origin + cover?.url} alt="" /> : <DefaultVideo />)}
+    </div>
+  );
 });
 
 export default VideoBlock;
